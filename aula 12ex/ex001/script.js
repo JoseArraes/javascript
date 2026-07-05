6b41c21a9a37cf7e5ea9220cca9b0f78f798b599
José Arraes <108028001+JoseArraes@users.noreply.github.com>
2026-07-05 14:29:17 -0300

    05-07

---
+++ aula 12ex/ex001/script.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var data = new Date()
+    var hora = 13//data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 6 && hora < 12) {
+        img.src = 'img/bomdia.jpg'
+        document.body.style.background = '#b9b3a4'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'img/boatarde.jpg'
+        document.body.style.background = '#655738'
+    } else {
+        img.src = 'img/boanoite.jpg'
+        document.body.style.background = '#2a2a20'
+    }
+}
+
