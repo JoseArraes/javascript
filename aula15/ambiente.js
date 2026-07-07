2b1705c956908a78125b4a6834252ff1d4a361ea
José Arraes <108028001+JoseArraes@users.noreply.github.com>
2026-07-07 11:18:01 -0300

    a

---
+++ aula15/ambiente.js
@@ -0,0 +1,3 @@
+let num = [5,4,3,6,9,9]
+
+console.log(`Nosso vetor pe o ${num.length}`)
---
+++ aula15/vetornatela.js
@@ -0,0 +1,11 @@
+let valores = [8,1,4,5,8,9]
+
+/*for(let pos=0;pos < valores.length;pos++){
+    console.log(valores[pos])
+}*/
+valores.sort()
+for(let pos in valores){
+    console.log(valores[pos])
+}
+
+console.log(valores.indexOf(4))
